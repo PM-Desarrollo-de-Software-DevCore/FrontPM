452281dcb96060a636dd3ac7313105a216e1071b
Bruno Vázquez Espinoza <141672024+A00838521@users.noreply.github.com>
2026-05-26 17:53:54 -0600

    Searchbar

--- src/app/Layout/topbar.tsx
+++ src/app/Layout/topbar.tsx
@@ -8,6 +8,7 @@ import { getUserCompletedTodayCount } from "@/services/taskService"
 import FramedAvatar from "@/components/ui/avatar/FramedAvatar"
 import { getUserProfileDetails } from "@/services/userService"
 import NotificationCenter from "@/components/ui/notifications/NotificationCenter"
+import GlobalSearchBar from "@/components/Layout/GlobalSearchBar"
 
 export default function Topbar() {
   const [open, setOpen] = useState(false)
@@ -81,28 +82,7 @@ export default function Topbar() {
         <div className="flex items-center gap-3 sm:gap-6">
 
      
-          <div className="hidden md:flex items-center border border-sidebar-border rounded-xl px-3 py-2 w-64 lg:w-80 bg-background">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 text-muted-foreground mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-4.35-4.35M16.65 10.5a6.15 6.15 0 11-12.3 0 6.15 6.15 0 0112.3 0z"
-              />
-            </svg>
-
-            <input
-              type="text"
-              placeholder="Search..."
-              className="bg-transparent outline-none text-sm w-full placeholder:text-muted-foreground"
-            />
-          </div>
+          <GlobalSearchBar />
 
           <NotificationCenter />
 
